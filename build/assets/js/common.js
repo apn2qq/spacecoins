6aad8b3b2f1787bc64707467ab096fd16bb5831d
apn2qq <apn2qq@gmail.com>
2018-01-14 18:04:39 +0300

    editing layout

--- build/assets/js/common.js
+++ build/assets/js/common.js
@@ -1,9 +1,9 @@
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 115) {
-        $('.coin-list__header').addClass('fixed');
-    } else {
-        $('.coin-list__header').removeClass('fixed');
-    }
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() >= 115) {
+//         $('.coin-list__header').addClass('fixed');
+//     } else {
+//         $('.coin-list__header').removeClass('fixed');
+//     }
 
-    console.log(window.scrollY);
-});
+//     console.log(window.scrollY);
+// });

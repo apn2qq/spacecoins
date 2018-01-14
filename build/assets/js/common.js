79444f99788133a105f1598057d2963eae59db0f
apn2qq <apn2qq@gmail.com>
2018-01-14 21:27:36 +0300

    code editing

--- build/assets/js/common.js
+++ build/assets/js/common.js
@@ -7,3 +7,47 @@
 
 //     console.log(window.scrollY);
 // });
+
+function init() {
+    // get json file with currencies
+    $.getJSON('https://api.coinmarketcap.com/v1/ticker/?limit=20', coinsOut);
+};
+
+function coinsOut(data) {
+    // print coins on page 
+
+    var out = " ";
+    for (const key in data) {
+        console.log(data[key].market_cap_usd);
+
+        out += '<div class="coins-list__row"><div class="container"><div class="row"><div class="col-lg-3"><div class="coins-list__row-coin">';
+        out += `<div class="coins-list__row-coin-position">
+        <span>${data[key].rank}</span>
+        </div>`;
+        out += `<div class="coins-list__row-coin-image">`;
+        out += ` <img src="assets/svg/${data[key].symbol}.svg" alt="" width="23" height="23">`;
+        out += `</div>`;
+        out += `<div class="coins-list__row-coin-name">
+        <span>${data[key].name}</span>
+        </div>`;
+        out += `</div></div>`;
+        out += `<div class="col-lg-3">`;
+        out += `<div class="coins-list__row-price">
+        <span>
+            <i>$</i>${data[key].price_usd}</span>
+            </div>`;
+        out += '</div>';
+        out += `<div class="col-lg-3">`;
+        out += `<div class="coins-list__row-market-cap"><span><i>$</i>${data[key].market_cap_usd}</span></div>`;
+        out += '</div>';
+        out += `<div class="col-lg-3">`;
+        out += `<div class="coins-list__row-change"><span>${data[key].percent_change_24h}</span></div>`;
+        out += `</div></div></div></div>`;
+    };
+
+    $('.coin-list__content').html(out);
+};
+
+$(document).ready(function () {
+    init();
+});

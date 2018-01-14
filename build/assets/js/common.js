8ca1006a20461d885368fe0e9ad35c5d7fe30a90
apn2qq <apn2qq@gmail.com>
2018-01-14 17:20:03 +0300

    initial commit

---
+++ build/assets/js/common.js
@@ -0,0 +1,9 @@
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 115) {
+        $('.coin-list__header').addClass('fixed');
+    } else {
+        $('.coin-list__header').removeClass('fixed');
+    }
+
+    console.log(window.scrollY);
+});

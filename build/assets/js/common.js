f5641420f0e83384d1cdeb9c17cd1288a605e5dd
apn2qq <apn2qq@gmail.com>
2018-01-17 14:38:57 +0300

    editing code

--- build/assets/js/common.js
+++ build/assets/js/common.js
@@ -6,32 +6,22 @@ function init() {
 function printCoinsCurrenciesOnPage(data) {
 
     let out = "";
-    for (const key in data) {
-        //console.log(data[key].market_cap_usd);
 
-        // out += '<div class="coins-list__row"><div class="container"><div class="row"><div class="col-lg-3"><div class="coins-list__row-coin">';
-        // out += `<div class="coins-list__row-coin-position">
-        // <span>${data[key].rank}</span>
-        // </div>`;
-        // out += `<div class="coins-list__row-coin-image">`;
-        // out += ` <img src="assets/svg/${data[key].symbol}.svg" alt="" width="23" height="23">`;
-        // out += `</div>`;
-        // out += `<div class="coins-list__row-coin-name">
-        // <span>${data[key].name}</span>
-        // </div>`;
-        // out += `</div></div>`;
-        // out += `<div class="col-lg-3">`;
-        // out += `<div class="coins-list__row-price">
-        // <span>
-        //     <i>$</i>${data[key].price_usd}</span>
-        //     </div>`;
-        // out += '</div>';
-        // out += `<div class="col-lg-3">`;
-        // out += `<div class="coins-list__row-market-cap"><span><i>$</i>${data[key].market_cap_usd}</span></div>`;
-        // out += '</div>';
-        // out += `<div class="col-lg-3">`;
-        // out += `<div class="coins-list__row-change"><span>${data[key].percent_change_24h}<img src="assets/svg/arrow-down-red.svg" alt="" width="5" height="7"></span></div>`;
-        // out += `</div></div></div></div>`;
+    // деление маркеткапа на тысячные
+    const numberWithCommas = (x) => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        let elements = data[i].market_cap_usd;
+        console.log(numberWithCommas(elements));
+    };
+
+    // print pages on coin
+
+    for (const key in data) {
+
+
 
         out += `<div class="coins-list__row">
             <div class="container">
@@ -55,7 +45,7 @@ function printCoinsCurrenciesOnPage(data) {
                                 <i>$</i>${data[key].price_usd}</span>
                         </div>
                     </div>
-                    <div class="col-3 col-lg-3 d-sm-none">
+                    <div class="col-3 col-lg-3">
                         <div class="coins-list__row-market-cap">
                             <span>
                                 <i>$</i>${data[key].market_cap_usd}</span>
@@ -74,15 +64,6 @@ function printCoinsCurrenciesOnPage(data) {
     };
 
     $('.coins-list__content').html(out);
-
-    // count values in footer info blocks
-
-    var totalMarketCap = 0;
-    var sum;
-    for (var i = 0; i < data.length; i++) {
-        sum += data[i];
-    }
-    console.log(sum);
 };
 
 
